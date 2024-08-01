b0e2be1efdc591f04ef1ddb93d5785fb3916bf44
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-01 11:31:20 +0530

    Create 1.4.cpp

---
+++ 1.4.cpp
@@ -0,0 +1,16 @@
+//Check if n is even or odd
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter n: ";
+  cin>>n;
+  if(n%2==0){
+    cout<<"Yes";
+  }
+  else{
+    cout<<"No";
+  }
+}
