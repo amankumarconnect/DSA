43ba3b4866c910776173a96377a19f5a43cbb529
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-01 11:15:05 +0530

    Update 1.1.cpp

--- Codes/1.1.cpp
+++ Codes/1.1.cpp
@@ -1,1 +1,13 @@
+//Add Two Numbers
 
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cout<<"Enter a: ";
+  cin>>a;
+  cout<<"Enter b: ";
+  cin>>b;
+  cout<<"Sum is: "<<a+b;
+}
