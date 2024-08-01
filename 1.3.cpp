0805782bcfc6776077940c7d5ad36fd95aa88649
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-01 11:22:30 +0530

    Create 1.3.cpp

---
+++ 1.3.cpp
@@ -0,0 +1,18 @@
+//Determine if a<b
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cout<<"Enter the value of a: ";
+  cin>>a;
+  cout<<"Enter the value of b: ";
+  cin>>b;
+  if(a<b){
+    cout<<"Yes";
+  }
+  else{
+    cout<<"No";
+  }
+}
