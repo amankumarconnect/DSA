7e5011462ddee873a0f8b43837762c61a129b618
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-09 18:21:48 +0530

    Create 3b ascii.cpp

---
+++ loveBabbarCourse/3b ascii.cpp
@@ -0,0 +1,16 @@
+// ASCII values
+#include <iostream>
+using namespace std;
+
+int main() {
+  char c;
+  cout << "Enter a character: ";
+  cin >> c;
+  if (c >= 'a' && c <= 'z')
+    cout << "lowercase";
+  else if (c >= 'A' && c <= 'Z')
+    cout << "uppercase";
+  else if (c >= '0' && c <= '9')
+    cout << "digit";
+  return 0;
+}
