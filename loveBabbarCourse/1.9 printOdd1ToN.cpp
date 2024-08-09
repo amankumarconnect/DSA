9a219b9ea9e7c234ceff746defd1d89d680671fc
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-09 17:05:29 +0530

    Create 1.9 printOdd1ToN.cpp

---
+++ loveBabbarCourse/1.9 printOdd1ToN.cpp
@@ -0,0 +1,13 @@
+//Print odd numbers between 1 to n
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter n: ";
+  cin>>n;
+  for(int i=1;i<=n;i+=2){
+    cout<<i<<endl;
+  }
+  return 0;
+}
