e2b3ba60172da117c2b7b92a5d844e477ae80217
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-10 09:28:38 +0530

    Create 4d pattern6.cpp

---
+++ loveBabbarCourse/4d pattern6.cpp
@@ -0,0 +1,20 @@
+// pattern 6
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+  int i = 1;
+  while (i <= n) {
+    int j = 1;
+    while (j <= i) {
+      cout << "*";
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+  return 0;
+}
