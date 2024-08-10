bb02c26fc72e4d2cbe648f0dc896fb8f2a8e5c61
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-10 11:53:40 +0530

    Create 5a bitwiseOperators.cpp

---
+++ loveBabbarCourse/5a bitwiseOperators.cpp
@@ -0,0 +1,14 @@
+// Bitwise operators
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cout<<"Enter a and b: ";
+  cin>>a>>b;
+  cout<<"a&b: "<<(a&b)<<endl;
+  cout<<"a|b: "<<(a|b)<<endl;
+  cout<<"a^b: "<<(a^b)<<endl;
+  cout<<"~a: "<<(~a)<<endl;
+  return 0;
+}
