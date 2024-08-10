9490fc293bd7e95851d772d88a004b87441a177f
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-10 18:31:14 +0530

    Create 7a complementOfBase10Integer.cpp

---
+++ loveBabbarCourse/7a complementOfBase10Integer.cpp
@@ -0,0 +1,16 @@
+// Complement of base 10 integer
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"Enter n: ";
+  cin>>n;
+  int mask = 1;
+  while(mask<=n){
+    n = n^mask;
+    mask = mask<<1;
+  }
+  cout<<n;
+  return 0;
+}
