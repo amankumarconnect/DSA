6469322ced7498329bd91c02f6c73f128442c121
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-10 10:42:12 +0530

    Create 4s pattern21.cpp

---
+++ loveBabbarCourse/4s pattern21.cpp
@@ -0,0 +1,25 @@
+// pattern 21
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+  int i = 0;
+  while (i < n) {
+    int j = 0;
+    while (j < i) {
+      cout << " ";
+      j++;
+    }
+    j = 0;
+    while (j < (n - i)) {
+      cout << "*";
+      j++;
+    }
+    cout << endl;
+    i++;
+  }
+  return 0;
+}
