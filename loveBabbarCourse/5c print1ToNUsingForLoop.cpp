358af6221594fbaa6acc052131c620856cc34054
Aman Kumar <aman.kumar.connect@gmail.com>
2024-08-10 16:42:36 +0530

    Create 5c print1ToNUsingForLoop.cpp

---
+++ loveBabbarCourse/5c print1ToNUsingForLoop.cpp
@@ -0,0 +1,13 @@
+// Print 1 to n using for loop
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Enter n: ";
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    cout<<i<<endl;
+  }
+  return 0;
+}
