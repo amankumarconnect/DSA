c6e6ec1570a7a1c298a27b4ec47ab73ce86ce76a
Aman Kumar <aman.kumar.connect@gmail.com>
2024-09-02 16:36:01 +0530

    Update 8b miniCalculator.cpp

--- loveBabbarCourse/8b miniCalculator.cpp
+++ loveBabbarCourse/8b miniCalculator.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int a,b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b: ";
+    cin>>b;
+    char op;
+    cout<<"Enter opertion: ";
+    cin>>op;
+    switch(op){
+        case '+':
+            cout<<a+b;
+            break;
+        case '-':
+            cout<<a-b;
+            break;
+        case '*':
+            cout<<a*b;
+            break;
+        case '/':
+            cout<<a/b;
+            break;
+        default:
+            cout<<"please enter a valid operation";
+    }
+}
