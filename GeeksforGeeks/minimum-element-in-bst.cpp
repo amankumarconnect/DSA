2a9f3687ea45e7a27fcd85477272ae4b4ac2e627
Aman Kumar <aman.kumar.connect@gmail.com>
2024-09-16 22:01:19 +0530

    Create minimum-element-in-bst.cpp

---
+++ GeeksforGeeks/minimum-element-in-bst.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+// Function to find the minimum element in the given BST.
+
+/* struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // Code here
+        Node* temp = root;
+        while(temp->left){
+            temp = temp->left;
+        }
+        return temp->data;
+    }
+};
