067c5039a438fda9eea0044a224e787c246140b1
Aman Kumar <aman.kumar.connect@gmail.com>
2024-09-17 19:04:58 +0530

    Create 94 binary-tree-inorder-traversal.cpp

---
+++ LeetCode/94 binary-tree-inorder-traversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* temp = root;
+        vector<int> vecs;
+        traversing(vecs,temp);
+        return vecs;
+    }
+
+    void traversing(vector<int> &vecs, TreeNode* temp){
+        if(!temp) return;
+        traversing(vecs,temp->left);
+        vecs.push_back(temp->val);
+        traversing(vecs,temp->right);
+    }
+};
