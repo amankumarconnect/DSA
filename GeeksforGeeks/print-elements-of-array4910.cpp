3035f8eecc7ef07f190852068163f1e4600c446d
Aman Kumar <aman.kumar.connect@gmail.com>
2024-09-20 21:47:14 +0530

    Create print-elements-of-array4910.cpp

---
+++ GeeksforGeeks/print-elements-of-array4910.cpp
@@ -0,0 +1,9 @@
+// User function template for C++
+class Solution {
+  public:
+    // Just print the space seperated array elements
+    void printArray(vector<int> &arr) {
+        // code here
+        for(int i:arr) cout<<i<<" ";
+    }
+};
