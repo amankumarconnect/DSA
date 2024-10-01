bbd063d1a1cfa3521e8820ffae540c67dd0be7a0
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-01 11:22:43 +0530

    Update set-kth-bit3724.cpp

--- GeeksforGeeks/set-kth-bit3724.cpp
+++ GeeksforGeeks/set-kth-bit3724.cpp
@@ -4,12 +4,7 @@ public:
     int setKthBit(int N, int K)
     {
         // Write Your Code here
-        int temp = N;
-        temp = temp>>K;
-        if(temp&1){
-            return N;
-        }
-        else return N+pow(2,K);
+        return (1<<K| N);
     }
     
 };
