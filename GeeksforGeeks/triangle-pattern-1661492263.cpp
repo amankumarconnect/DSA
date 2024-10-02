5c4c134c8b04869f2cba1ae82e87f4d586005575
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-02 09:28:34 +0530

    Update triangle-pattern-1661492263.cpp

--- GeeksforGeeks/triangle-pattern-1661492263.cpp
+++ GeeksforGeeks/triangle-pattern-1661492263.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i = 0; i<n; i++){
+            for(int j = 1; j<n-i;j++){
+                cout<<" ";
+            }
+            for(int k = 0; k<=2*i; k++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
