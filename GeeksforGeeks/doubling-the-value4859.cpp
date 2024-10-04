e3e2526034458415b2d84825f2821e7ce038bc7e
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-04 22:46:55 +0530

    Create doubling-the-value4859.cpp

---
+++ GeeksforGeeks/doubling-the-value4859.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    long long solve(int n, long long a[], long long b)
+    {
+        //code here.
+        for(int i = 0; i<n; i++){
+            if(a[i]==b) b*=2;
+        }
+        return b;
+    }  
+};
