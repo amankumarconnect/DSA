fd2e3622c326b8c0c0010c59da2120251a203752
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-10 10:42:59 +0530

    Create c-pointers-set-1introduction.cpp

---
+++ GeeksforGeeks/c-pointers-set-1introduction.cpp
@@ -0,0 +1,12 @@
+
+
+/* This function is invoked by passing the address of the variable.
+    You need to increment the value stored in the variable by 10 */
+
+/* complete the function below */
+
+void updateVar(int *a)
+{
+      // write your code here
+      *a +=10;
+}
