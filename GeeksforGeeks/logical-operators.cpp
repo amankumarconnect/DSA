97c5360fbb762ff91fe3465cda828ff6a9f58d11
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-15 23:05:35 +0530

    Create logical-operators.cpp

---
+++ GeeksforGeeks/logical-operators.cpp
@@ -0,0 +1,8 @@
+//User function Template for C++
+
+
+void logicOp(int a, int b)
+{
+    /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/ 
+    cout<<(a&&b)<<" "<<(a||b)<<" "<<((!a)&&(!b))<<endl;
+}
