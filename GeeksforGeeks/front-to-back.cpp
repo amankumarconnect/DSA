04e62d072aaf06529a630a4922622e9282f74380
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-20 13:04:33 +0530

    Create front-to-back.cpp

---
+++ GeeksforGeeks/front-to-back.cpp
@@ -0,0 +1,12 @@
+//User function Template for C++
+
+//Complete this function. it1 points to vector.begin(), it2 points to vector.end()
+void iter(vector<int>::iterator it1,vector<int>::iterator it2)
+{
+    //Your code here. Use iterator to print all the elements
+    while(it1!=it2){
+        cout<<*it1<<" ";
+        it1+=1;
+    }
+    cout<<endl;
+}
