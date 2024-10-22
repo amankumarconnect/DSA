2bc5c153c9f36b834d5353184606bd04b2ce5296
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-22 09:41:07 +0530

    Create c-data-types1523.cpp

---
+++ GeeksforGeeks/c-data-types1523.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int cppIntType() {
+        // code here
+        int n;
+        cin>>n;
+        return n;
+    }
+    
+    char cppCharType() {
+        // code here
+        char c;
+        cin>>c;
+        return c;
+    }
+    
+    float cppFloatType() {
+        // code here
+        float f;
+        cin>>f;
+        return f;
+    }
+};
