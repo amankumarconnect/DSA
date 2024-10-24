2b37eb30c54a486d3f48a814e155d52e5c2aec32
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-24 08:50:46 +0530

    Create c-operators-relational-set-21407.cpp

---
+++ GeeksforGeeks/c-operators-relational-set-21407.cpp
@@ -0,0 +1,13 @@
+
+class Solution{   
+public:
+    string compareNum(int A,int B){
+        // code here 
+        if(A>B){
+            return to_string(A) + " is greater than " + to_string(B);
+        }
+        else if(A<B) return to_string(A) + " is less than " + to_string(B);
+        
+        return to_string(A) + " is equals to " + to_string(B);
+    }
+};
