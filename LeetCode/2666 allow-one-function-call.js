f19ed7fe3617cac879ce594fc38a9180545eda8d
Aman Kumar <aman.kumar.connect@gmail.com>
2024-10-26 19:34:41 +0530

    Create 2666 allow-one-function-call.js

---
+++ LeetCode/2666 allow-one-function-call.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let n = 1;
+    return function(...args){
+        if(n === 1){
+            n++;
+            return fn(...args);
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
