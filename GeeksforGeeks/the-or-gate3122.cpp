fdd16918a98439ef2570fac328fdfeead39006ec
Aman Kumar <aman.kumar.connect@gmail.com>
2024-11-10 22:09:19 +0530

    Create the-or-gate3122.cpp

---
+++ GeeksforGeeks/the-or-gate3122.cpp
@@ -0,0 +1,11 @@
+   
+class Solution{
+public:
+    int orGate(vector<int> arr,int N){
+        // code here 
+        for(int i: arr){
+            if(i==1) return 1;
+        }
+        return 0;
+    }
+};
